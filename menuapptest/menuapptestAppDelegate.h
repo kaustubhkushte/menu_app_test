4e3579e9fd7bda532e48a98f3b050523e206823e
kaustubh <kaustubhkushte@gmail.com>
2011-11-03 09:28:32 +0530

    Initial Commit

---
+++ menuapptest/menuapptestAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  menuapptestAppDelegate.h
+//  menuapptest
+//
+//  Created by Kaustubh Kushte on 03/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class menuapptestViewController;
+
+@interface menuapptestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet menuapptestViewController *viewController;
+
+@end
---
+++ menuapptest/menuapptestViewController.h
@@ -0,0 +1,15 @@
+//
+//  menuapptestViewController.h
+//  menuapptest
+//
+//  Created by Kaustubh Kushte on 03/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface menuapptestViewController : UIViewController {
+    
+}
+
+@end
