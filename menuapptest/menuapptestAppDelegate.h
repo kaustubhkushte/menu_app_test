9ed088d8be48aed7a3fea431d8bb47e17d394ed6
Kaustubh Kushte <kaustubhkushte@gmail.com>
2011-11-04 14:05:33 +0530

    Hello World

--- menuapptest/menuapptestAppDelegate.h
+++ menuapptest/menuapptestAppDelegate.h
@@ -3,7 +3,7 @@
 //  menuapptest
 //
 //  Created by Kaustubh Kushte on 03/11/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 experience commerce. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
